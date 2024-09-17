12b9b4c321519dd2242657e9836a43ef8b3e78f8
Md. Saif Ali <159515806+mdsaifalidev@users.noreply.github.com>
2024-09-17 09:55:57 +0530

    Update MyBlog.jsx

--- frontend/src/pages/blog/MyBlog.jsx
+++ frontend/src/pages/blog/MyBlog.jsx
@@ -34,17 +34,17 @@ const MyBlog = () => {
           {blogs.length > 0 ? (
             blogs.map((blog) => (
               <div key={blog._id} className="group">
-                <div className="max-w-sm h-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div className="max-w-sm h-full bg-white border border-gray-200 rounded-lg shadow">
                   <img
                     className="rounded-t-lg w-full"
                     alt={blog.image}
                     src={blog.image}
                   />
                   <div className="p-5">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
                       {blog.title}
                     </h5>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    <p className="mb-3 font-normal text-gray-700">
                       {blog.content}
                     </p>
                     {blog.status === "rejected" && (
